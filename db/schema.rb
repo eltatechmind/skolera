5e844ecb42a5a2e5e4093b328008a1987fdc26ee
Elta3lab <ahmedfouad2020era@gmail.com>
2019-06-10 19:47:16 +0200

    create teacher model, migration, spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_172514) do
+ActiveRecord::Schema.define(version: 2019_06_10_174624) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
