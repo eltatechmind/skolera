767fa6cb20519dea96710fce419619e7cb405397
Elta3lab <ahmedfouad2020era@gmail.com>
2019-06-10 19:53:08 +0200

    create course model, migration, spec files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_174624) do
+ActiveRecord::Schema.define(version: 2019_06_10_174837) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "name"
