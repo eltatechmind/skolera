f4e9d9deb960e217de42951e4205ea50d59c667a
Elta3lab <ahmedfouad2020era@gmail.com>
2019-06-15 19:14:47 +0200

    fix paperclip storing files on production

--- app/models/csv.rb
+++ app/models/csv.rb
@@ -1,4 +1,6 @@
 class Csv < ApplicationRecord
   has_attached_file :csv
   validates_attachment :csv, content_type: { content_type: "text/csv" }
+  has_attached_file :auto, :storage => :cloudinary,
+  :path => ':id/:style/:filename'
 end
