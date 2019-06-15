d5a511fd94c4c526ee14c46db0205273d2ed205c
Elta3lab <ahmedfouad2020era@gmail.com>
2019-06-15 20:36:45 +0200

    paperclip store

--- app/models/csv.rb
+++ app/models/csv.rb
@@ -1,8 +1,4 @@
 class Csv < ApplicationRecord
-  if !Rails.env.production?
-    has_attached_file :csv
-  else  
-    has_attached_file :csv, :storage => :cloudinary
-  end
+  has_attached_file :csv, :path => 'app/storage/:id/:style/:filename'
   validates_attachment :csv, content_type: { content_type: "text/csv" }
 end
