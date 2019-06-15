ada62d7f242aabf78a20b4467ead52b45451776f
Elta3lab <ahmedfouad2020era@gmail.com>
2019-06-15 19:33:22 +0200

    edit cloudinary paperclip saving directory

--- app/models/csv.rb
+++ app/models/csv.rb
@@ -2,5 +2,5 @@ class Csv < ApplicationRecord
   has_attached_file :csv
   validates_attachment :csv, content_type: { content_type: "text/csv" }
   has_attached_file :auto, :storage => :cloudinary,
-  :path => ':id/:style/:filename'
+  :path => 'myfiles'
 end
