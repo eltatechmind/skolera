a876657c3368744a7228a8e4a827ed084096a4c4
Elta3lab <ahmedfouad2020era@gmail.com>
2019-06-15 18:57:56 +0200

    hotfix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0'  }
-end
+if !Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://localhost:6379/0'  }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0'  }
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://localhost:6379/0'  }
+  end
 end
