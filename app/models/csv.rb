beb67fa0db8d7e1f570151cadabfb19f0f0041ab
Elta3lab <ahmedfouad2020era@gmail.com>
2019-06-15 19:53:01 +0200

    fix paperclip cloudinary

--- app/models/csv.rb
+++ app/models/csv.rb
@@ -1,6 +1,9 @@
 class Csv < ApplicationRecord
-  has_attached_file :csv
+  if !Rails.env.production?
+    has_attached_file :csv
+  else  
+    has_attached_file :csv, :storage => :cloudinary,
+    :path => ':id/:style/:filename'
+  end
   validates_attachment :csv, content_type: { content_type: "text/csv" }
-  has_attached_file :auto, :storage => :cloudinary,
-  :path => 'myfiles'
 end
