1eb5589845d73659fd73fd1042396bad730abefc
Elta3lab <ahmedfouad2020era@gmail.com>
2019-06-15 21:22:27 +0200

    hotfix cloudinary

--- app/models/csv.rb
+++ app/models/csv.rb
@@ -1,4 +1,8 @@
 class Csv < ApplicationRecord
-  has_attached_file :csv, :path => 'app/storage/:id/:style/:filename'
+  if Rails.env.production?
+    has_attached_file :raw, :storage => :cloudinary, :path => 'public/system/csvs/csvs/000/000/:id/:style/:filename'
+  else
+    has_attached_file :csv, :path => 'public/system/csvs/csvs/000/000/:id/:style/:filename'
+  end
   validates_attachment :csv, content_type: { content_type: "text/csv" }
 end
