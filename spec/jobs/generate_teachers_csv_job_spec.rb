5c14df518906ca4857166201eec993df4d3f5c7f
Elta3lab <ahmedfouad2020era@gmail.com>
2019-06-15 02:21:50 +0200

    finish specs for jobs

--- spec/jobs/generate_teachers_csv_job_spec.rb
+++ spec/jobs/generate_teachers_csv_job_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe GenerateTeachersCsvJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Should run the job" do
+    ActiveJob::Base.queue_adapter = :test
+    expect {
+      GenerateTeachersCsvJob.perform_later
+    }.to have_enqueued_job(GenerateTeachersCsvJob)
+  end
+  it "Should increase Csv Model records by 1" do
+    expect { GenerateTeachersCsvJob.perform_now }.to change { Csv.where(kind: "teachers").count }.by(1)
+  end
 end
