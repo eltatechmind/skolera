49dbf2d3379606f6004801810833c3959348b5b8
Elta3lab <ahmedfouad2020era@gmail.com>
2019-06-15 22:28:44 +0200

    hotfix

--- Gemfile
+++ Gemfile
@@ -63,6 +63,5 @@ group :production do
 end
 
 gem 'paperclip'
-gem 'paperclip-cloudinary'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/models/csv.rb
+++ app/models/csv.rb
@@ -1,4 +1,4 @@
 class Csv < ApplicationRecord
-  has_attached_file :csv, :path => 'public/system/csvs/csvs/000/000/:id/:style/:filename'
+  has_attached_file :csv
   validates_attachment :csv, content_type: { content_type: "text/csv" }
 end
